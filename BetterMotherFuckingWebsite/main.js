f5ecce729b8b988d040b9e44206688dfd5cf2f9a
jerome delfosse <jeromedelfosse@macbook-pro-de-jerome.home>
2018-09-22 17:53:58 +0200

    BetterMotherFuckingWebsite

---
+++ BetterMotherFuckingWebsite/main.js
@@ -0,0 +1,8 @@
+function popup()
+{
+	if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'))
+	document.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
+setTimeout(popup, 10000);
+
+<script src="scripts/main.js"></script>
